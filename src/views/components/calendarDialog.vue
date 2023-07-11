47ba32c6de91fb2b71cbd39ad8c0a110f92a2a40
kodak <kodakjerec@hotmail.com>
2023-07-11 15:27:49 +0800

    update ts error

--- src/views/components/calendarDialog.vue
+++ src/views/components/calendarDialog.vue
@@ -4,23 +4,23 @@
         <div class="fixed left-0 top-0 w-full h-full bg-slate-100 opacity-90" @click="clickWhiteSpace">
         </div>
         <div class="fixed top-10 w-full text-center px-6">
-            <div class="p-2 bg-white rounded border border-black" :class="{'shockWindow':shockWindow}">
+            <div class="p-2 bg-white rounded border border-black" :class="{ 'shockWindow': shockWindow }">
                 <div id="model_header" class="relative justify-center">
                     <p class="text-2xl font-bold mt-2 text-center">{{ myTitle }}</p>
-                    <close class="absolute top-0 right-0" theme="filled" size="24" fill="#000000" @click="closeDialog('')"/>
+                    <close class="absolute top-0 right-0" theme="filled" size="24" fill="#000000" @click="closeDialog('')" />
                 </div>
                 <div id="model_content">
                     <template v-for="item of recordingTable" :key="item.id">
                         <label :for="item.colName" class="text-gray-700 flex mt-2">
                             <span class="label-xl w-1/3">{{ item.colName }}</span>
-                            <input v-if="item.colType==='datetime-local'" :id="item.colName" class="input text-lg w-1/2" v-model="recorder[item.colName]" @focus="$event.target?.select()" disabled>
-                            <input v-else :type="item.colType" :id="item.colName" class="input w-1/2" v-model="recorder[item.colName]" @focus="$event.target?.select()" disabled>
+                            <input v-if="item.colType === 'datetime-local'" :id="item.colName" class="input text-lg w-1/2" v-model="recorder[item.colName]" @focus="inputFocus($event)" disabled>
+                            <input v-else :type="item.colType" :id="item.colName" class="input w-1/2" v-model="recorder[item.colName]" @focus="inputFocus($event)" disabled>
                         </label>
                     </template>
                 </div>
                 <div id="model_footer">
                     <div class="m-2" @click="deleteItem">
-                        <delete theme="filled" size="24" fill="#FF0000"/>
+                        <delete theme="filled" size="24" fill="#FF0000" />
                         <span class="text-red-500">Delete</span>
                     </div>
                 </div>
@@ -33,7 +33,7 @@
 import { Close, Delete } from "@icon-park/vue-next";
 import { storeSettings, type recordModule } from '@/store'
 
-export default{
+export default {
     name: "calendarDialog",
     components: {
         Close, Delete
@@ -51,7 +51,7 @@ export default{
     data() {
         return {
             myTitle: this.title,
-            recordingTable: [] as Array<recordModule> ,
+            recordingTable: [] as Array<recordModule>,
             recorder: {} as Object,
             shockWindow: false
         }
@@ -70,25 +70,29 @@ export default{
         clickWhiteSpace() {
             this.shockWindow = true;
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.shockWindow = false;
                 this.myTitle = "一秒後關閉視窗";
-            },50);
+            }, 50);
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.closeDialog("");
-            },1000);
+            }, 1000);
         },
         // 刪除資料
         async deleteItem() {
             const result = await this.$swal({
-                title:'刪除此筆紀錄？',
+                title: '刪除此筆紀錄？',
                 showCancelButton: true,
-                confirmButtonText: 'Ok'});
+                confirmButtonText: 'Ok'
+            });
 
             if (result.isConfirmed) {
                 this.$emit('delete', this.fromData.id);
             }
+        },
+        inputFocus(event: any) {
+            event.target.select();
         }
     }
 }
