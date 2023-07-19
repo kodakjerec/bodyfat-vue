8180ba0e2124fb69f6f83a43b655bc7ef287138b
TAIPEI\mPOS109 <kodakjerec@gmail.com>
2023-07-19 11:50:11 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -11,16 +11,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  server: {
-    //主要是加上这段代码
-    host: "127.0.0.1",
-    port: 80,
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1", //实际请求地址
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  build: {
+    target: 'esnext'
+  }
 });
